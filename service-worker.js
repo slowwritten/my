f9f748712463f0cfa921a5edebdd0790cc524f13
slowwritten <97490703+slowwritten@users.noreply.github.com>
2026-06-18 17:32:41 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 self.addEventListener("install", (e) => {
-  self.skipWaiting(); // 바로 활성화
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => caches.delete(key)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request)); // 항상 최신 요청
+  e.respondWith(fetch(e.request));
 });
